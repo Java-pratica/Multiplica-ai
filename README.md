8cc1a7cce0d0f75e9d59acaa48fa22df784dc925
Driven Education <yessica_stern@hotmail.com>
2023-03-24 21:50:02 -0300

    Print the multiples

---
+++ README.md
@@ -0,0 +1,2 @@
+## Objetivo:
+Código que imprime uma tabela contendo os múltiplos de 1-10 do número fornecido
---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args){
+        int value = 4;
+        System.out.println("Tabela de multiplicação de "+ value);
+        for(int i=1; i<11; i++){
+            System.out.println(value + " x " +  i + " = " + value *i);
+        }
+    }
+}
